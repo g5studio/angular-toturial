bc1c3769abdc14912042343696655e4553632a48
william.chiang <william.chiang@tcghl.com>
2021-03-09 17:38:40 +0800

    initial style

--- src/app/modules/layout/layout.module.ts
+++ src/app/modules/layout/layout.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './pages/layout/layout.component';
+import { HeaderComponent } from './components/header/header.component';
 
 
 
 @NgModule({
-  declarations: [LayoutComponent],
+  declarations: [
+    LayoutComponent,
+    HeaderComponent
+  ],
+  exports: [
+    LayoutComponent
+  ],
   imports: [
     CommonModule
   ]
